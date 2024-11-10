78ade240c15b85b37a9ed5a0d6ab9b839511059a
Dasendhran Subramoney <das.subramoney@gmail.com>
2024-11-10 18:33:08 +0200

    Added component

--- Components/Booklist.js
+++ Components/Booklist.js
@@ -15,8 +15,172 @@ class BookList extends HTMLElement {
     render() {
         this.shadowRoot.innerHTML = `
             <style>
-                /* Add your CSS styles here */
+                .wrapper{
+                    position: fixed;
+                    bottom: 0;
+                    left: 0;
+                    width: 100%;
+                    border-width: 0;
+                    box-shadow: 0px 3px 3px -2px rgba(0,0,0,0.2), 0px 3px 4px 0px rgba(0,0,0,0.14), 0px 1px 8px 0px rgba(0,0,0,0.12);
+                    animation-name: enter;
+                    animation-duration: 0.6s;
+                    z-index: 10;
+                    background-color: rgba(var(--color-light), 1);
+                    }
+
+                    @media (min-width: 30rem) {
+                    .wrapper {
+                        max-width: 30rem;
+                        left: 0%;
+                        top: 0;
+                        border-radius: 8px;;
+                    }
+                    }
+
+                    .form {
+                    padding-bottom: 0.5rem;
+                    margin: 0 auto;
+                    }
+
+                    .row {
+                    display: flex;
+                    gap: 0.5rem;
+                    margin: 0 auto;
+                    justify-content: center;
+                    }
+
+                    .button {
+                    font-family: Roboto, sans-serif;
+                    background-color: rgba(var(--color-blue), 0.1);
+                    transition: background-color 0.1s;
+                    border-width: 0;
+                    border-radius: 6px;
+                    height: 2.75rem;
+                    cursor: pointer;
+                    width: 50%;
+                    color: rgba(var(--color-blue), 1);
+                    font-size: 1rem;
+                    border: 1px solid rgba(var(--color-blue), 1);
+                    }
+
+                    .button_primary {
+                    background-color: rgba(var(--color-blue), 1);
+                    color: rgba(var(--color-force-light), 1);
+                    }
+
+                    .button:hover {
+                    background-color: rgba(var((var(--color-blue))), 0.2);
+                    }
+
+
+                    .button_primary:hover {
+                    background-color: rgba(var(--color-blue), 0.8);
+                    color: rgba(var(--color-force-light), 1);
+                    }
+
+                    .input {
+                    width: 100%;
+                    margin-bottom: 0.5rem;
+                    background-color: rgba(var(--color-dark), 0.05);
+                    border-width: 0;
+                    border-radius: 6px;
+                    width: 100%;
+                    height: 4rem;
+                    color: rgba(var(--color-dark), 1);
+                    padding: 1rem 0.5rem 0 0.75rem;
+                    font-size: 1.1rem;
+                    font-weight: bold;
+                    font-family: Roboto, sans-serif;
+                    cursor: pointer;
+                    }
+
+                    .input_select {
+                    padding-left: 0.5rem;
+                    }
+
+                    .field {
+                    position: relative;
+                    display: block;
+                    }
+
+                    .label {
+                    position: absolute;
+                    top: 0.75rem;
+                    left: 0.75rem;
+                    font-size: 0.85rem;
+                    color: rgba(var(--color-dark), 0.4);
+                    }
+
+                    .input:hover {
+                    background-color: rgba(var(--color-dark), 0.1);
+                    }
+
+                    .title {
+                    padding: 1rem 0 0.25rem;
+                    font-size: 1.25rem;
+                    font-weight: bold;
+                    line-height: 1;
+                    letter-spacing: -0.1px;
+                    max-width: 25rem;
+                    margin: 0 auto;
+                    color: rgba(var(--color-dark), 0.8)
+                    }
+
+                    .blur {
+                    width: 100%;
+                    height: 200px;
+                    filter: blur(10px);
+                    opacity: 0.5;
+                    transform: scale(2);
+                    }
+
+                    .image {
+                    max-width: 10rem;
+                    position: absolute;
+                    top: 1.5m;
+                    left: calc(50% - 5rem);
+                    border-radius: 2px;
+                    box-shadow: 0px 3px 3px -2px rgba(0,0,0,0.2), 0px 3px 4px 0px rgba(0,0,0,0.14), 0px 1px 8px 0px rgba(0,0,0,0.12);
+                    }
+
+                    .data {
+                    font-size: 0.9rem;
+                    display: -webkit-box;
+                    -webkit-line-clamp: 6;
+                    -webkit-box-orient: vertical;  
+                    overflow: hidden;
+                    color: rgba(var(--color-dark), 0.8)
+                    }
+
+                    .data_secondary {
+                    color: rgba(var(--color-dark), 0.6)
+                    }
+
+                    .content {
+                    padding: 2rem 1.5rem;
+                    text-align: center;
+                    padding-top: 3rem;
+                    }
+
+                    .preview {
+                    overflow: hidden;
+                    margin: -1rem;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    }
+
+                    .background {
+                    background: rgba(var(--color-dark), 0.6);
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    height: 100vh;
+                    width: 100vw;
+                    }
             </style>
+
+            
             <div>
                 <genre-options></genre-options>
                 <author-options></author-options>
