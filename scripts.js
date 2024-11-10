d6296cf3e053429e69977b3288a6e8d67b43a015
Dasendhran Subramoney <das.subramoney@gmail.com>
2024-11-10 22:16:06 +0200

    Updated

--- scripts.js
+++ scripts.js
@@ -9,86 +9,9 @@ class BookList {
     }
 
     intializelist(){
-        this.renderBookPreviews();
-        this.renderGenreOptions();
-        this.renderAuthorOptions();
-        this.applyTheme();
-        this.setButtonState();
         this.addEventListeners();
     }
 
-    renderBookPreviews() { // Method for sorting book reviews 
-        const starting = document.createDocumentFragment();
-        for (const { author, id, image, title } of this.matches.slice(0, BOOKS_PER_PAGE)) {
-            const element = this.createBookPreviewElement(id, image, title, author);
-            starting.appendChild(element);
-        }
-        document.querySelector('[data-list-items]').appendChild(starting);
-    }
-
-    createBookPreviewElement(id, image, title, author) { // Function takes in the input valuesa and adds then to the HTML
-        const element = document.createElement('button');
-        element.classList = 'preview';
-        element.setAttribute('data-preview', id);
-        element.innerHTML = `
-            <img class="preview__image" src="${image}" />
-            <div class="preview__info">
-                <h3 class="preview__title">${title}</h3>
-                <div class="preview__author">${authors[author]}</div>
-            </div>
-        `;
-        return element; // Result is being returned
-    }    
-
-    renderGenreOptions() { // Method goes through the different genres in the database 
-        const genreHtml = document.createDocumentFragment();
-        const firstGenreElement = this.createOptionElement('any', 'All Genres');
-        genreHtml.appendChild(firstGenreElement);
-        for (const [id, name] of Object.entries(genres)) {
-            genreHtml.appendChild(this.createOptionElement(id, name));
-        }
-        document.querySelector('[data-search-genres]').appendChild(genreHtml);
-    }
-
-    renderAuthorOptions() { // Method goes through the different authors in the database
-        const authorsHtml = document.createDocumentFragment();
-        const firstAuthorElement = this.createOptionElement('any', 'All Authors');
-        authorsHtml.appendChild(firstAuthorElement);
-        for (const [id, name] of Object.entries(authors)) {
-            authorsHtml.appendChild(this.createOptionElement(id, name));
-        }
-        document.querySelector('[data-search-authors]').appendChild(authorsHtml);
-    }
-
-    createOptionElement(id, name) { // Function takes in the input valuesa and adds then to the HTML
-        const element = document.createElement('option');
-        element.value = id;
-        element.innerText = name;
-        return element; // Result is being returned
-    }
-
-    applyTheme() { // Add ths theme when a condition is met 
-        const theme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'night' : 'day';
-        document.querySelector('[data-settings-theme]').value = theme;
-        this.updateTheme(theme);
-    }
-
-    updateTheme(theme) { // Updates the theme with the condition being met 
-        const dark = theme === 'night' ? '255, 255, 255' : '10, 10, 20';
-        const light = theme === 'night' ? '10, 10, 20' : '255, 255, 255';
-        document.documentElement.style.setProperty('--color-dark', dark);
-        document.documentElement.style.setProperty('--color-light', light);
-    }
-
-    // Set button method
-    setButtonState() {
-        const remaining = this.matches.length - BOOKS_PER_PAGE;
-        document.querySelector('[data-list-button]').innerHTML = `
-            <span>Show more</span>
-            <span class="list__remaining"> (${remaining > 0 ? remaining : 0})</span>
-        `;
-        document.querySelector('[data-list-button]').disabled = remaining <= 0;
-    }
 
     addEventListeners() { // Method which holds all the event listeners 
         document.querySelector('[data-search-cancel]').addEventListener('click', () => this.closeOverlay('[data-search-overlay]'));
@@ -180,15 +103,6 @@ class BookList {
 
         newItems.appendChild(element)
         }
-    }
-
-
-    loadMoreBooks() { // Method for the list button  
-        const fragment = document.createDocumentFragment();
-        this.matches.slice(this.page * BOOKS_PER_PAGE, (this.page + 1) * BOOKS_PER_PAGE).forEach((book) => fragment.appendChild(this.createBookPreviewElement(book.id, book.image, book.title, book.author)));
-        document.querySelector('[data-list-items]').appendChild(fragment);
-        this.page += 1;
-        this.setButtonState();
     }
 
     showBookDetails(event) { // Method for the list items 
